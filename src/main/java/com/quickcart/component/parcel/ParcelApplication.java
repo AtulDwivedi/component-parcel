f5e5b476a1336467315d9de42916244aed6cf1c5
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-03-10 11:07:09 +0530

    initial commit

---
+++ src/main/java/com/quickcart/component/parcel/ParcelApplication.java
@@ -0,0 +1,14 @@
+package com.quickcart.component.parcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ParcelApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelApplication.class, args);
+	}
+}
---
+++ src/main/java/com/quickcart/component/parcel/ParcelController.java
@@ -0,0 +1,23 @@
+package com.quickcart.component.parcel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/rest/v1/parcels")
+public class ParcelController {
+
+    @Value("${parcel.modes}")
+    private List<String> parcelModes;
+
+    @GetMapping("/modes")
+    public String getAvailableParcelOptions() {
+        return parcelModes.get(new Random().nextInt(3 - 0 + 1) + 0);
+    }
+}
+
